bff64b763bd80cb57568fb81c90df8218e71c832
Jean Paiva <paivajean263@gmail.com>
2026-08-30 21:46:47 -0300

    cc

--- frontend/src/components/common/auth/AuthModal.jsx
+++ frontend/src/components/common/auth/AuthModal.jsx
@@ -6,44 +6,34 @@ import { useContext } from "react";
 import { AuthContext } from "../../../context/AuthProvider";
 import BtnClose from "../BtnClose";
 
-function AuthModal()
-{
-    const {
-    isAuthModalOpen,
-    setIsAuthModalOpen
-} = useContext(AuthContext);
-    const [mode, setMode] = useState("login");
+function AuthModal() {
+  const { isAuthModalOpen, setIsAuthModalOpen } = useContext(AuthContext);
+  const [mode, setMode] = useState("login");
 
-    if(!isAuthModalOpen)
-    {
-        return null;
-    }
+  if (!isAuthModalOpen) {
+    return null;
+  }
 
-    function handleClose() {
+  function handleClose() {
     setMode("login");
     setIsAuthModalOpen(false);
-}
-    return(
-        <div className="auth-overlay">
-           <div className={`auth-modal ${mode === "register" ? "fullscreen" : ""}`}>
-             <BtnClose onClick={handleClose}/>
-                {
-                    mode==="login" &&(
-                       <LoginForm onRegister={()=> setMode("register")} />
+  }
+  return (
+    <div className="auth-overlay">
+      <div className={`auth-modal ${mode === "register" ? "fullscreen" : ""}`}>
+        <BtnClose onClick={handleClose} />
+        <div className="container">
+          {mode === "login" && (
+            <LoginForm onRegister={() => setMode("register")} />
+          )}
 
-
-                    )
-                }       
-
-                {
-                    mode==="register" &&(
-                        <RegisterForm onLogin={()=> setMode("login")} />
-                    )
-                } 
-            </div>
-
+          {mode === "register" && (
+            <RegisterForm onLogin={() => setMode("login")} />
+          )}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default AuthModal;
