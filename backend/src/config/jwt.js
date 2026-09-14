7eff8f3b675f1f8b76aada41d9cbd8141a8162d6
Jean Paiva <paivajean263@gmail.com>
2026-09-14 15:49:55 -0300

    dd

---
+++ backend/src/config/jwt.js
@@ -0,0 +1,7 @@
+import "jsonwebtoken";
+
+export function generarToken(payload) {
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+}
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import { autenticarUsuario } from "../services/usuario.service.js";
+import { generarToken } from "../config/jwt.js";
 
 export async function login(req, res) {
   try {
@@ -6,9 +7,31 @@ export async function login(req, res) {
 
     const usuario = await autenticarUsuario(email, password);
 
+    const payload = {
+      idUsuario: usuario.idusuario,
+      idEmpleado: usuario.idempleado,
+      idCliente: usuario.idcliente,
+      idRol: usuario.idrol,
+      rol: usuario.rol,
+    };
+
+    const token = generarToken(payload);
+
     res.status(200).json({
       mensaje: "Inicio sesion correcto",
-      usuario,
+      token,
+      usuario: {
+        idUsuario: usuario.idusuario,
+        email: usuario.email,
+        idEmpleado: usuario.idempleado,
+        idCliente: usuario.idcliente,
+        idRol: usuario.idrol,
+        nombreEmpleado: usuario.nombreempleado,
+        apellidoEmpleado: usuario.apellidoempleado,
+        nombreCliente: usuario.nombrecliente,
+        apellidoCliente: usuario.apellidocliente,
+        foto: usuario.foto,
+      },
     });
   } catch (error) {
     res.status(401).json({
